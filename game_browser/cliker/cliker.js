9e63b54fb5438fce6f920bc67dcd1f2687500559
Gamespace2009 <mazuninandrej85@gmail.com>
2026-07-27 14:06:02 +0500

    update cliker

--- game_browser/cliker/cliker.js
+++ game_browser/cliker/cliker.js
@@ -2,9 +2,10 @@ var q = 0;
 var y = 1;
 var i = 0;
 var p = 100;
+var e = 10;
 var skore = document.getElementById("score");
 var phon = document.getElementById("one");
-var kometa = ["img/разгорание.png", "img/разгорание X2.png", "img/разгорание X3.png", "img/сверхнова.png",]
+var kometa = ["img/разгорание.png", "img/разгорание X2.png", "img/разгорание X3.png", "img/сверхнова.png",];
 function scores() {
     q += y;
     skore.innerHTML = `${q}`;
@@ -12,9 +13,15 @@ function scores() {
     console.log(q);
 }
 function level_up(){
-    if (y == 1) y--
-    y += 10;
-    p*= 100
+    if (y > 1) {
+        y*= e;
+        e *= 10;
+    }
+    if (y == 1) {
+        y--;
+        y += 10;
+    }
+    p*= 100;
     if (i < kometa.length) { 
         phon.style.backgroundImage = `url(${kometa[i]})`;
         phon.style.backgroundRepeat = "no-repeat";
