94b82459605be2e659c2042f3531f8838eee310f
Gamespace2009 <mazuninandrej85@gmail.com>
2026-08-05 00:35:59 +0500

    крестики нолики обновление

--- game_browser/tic-tuc-toe/tic-tuc-toe.js
+++ game_browser/tic-tuc-toe/tic-tuc-toe.js
@@ -23,13 +23,13 @@ function hod(el){
     
     if (el.dataset.value == "3") {
         if (hd == 0) {
-            el.style.background = "url('img/нолик.png')";
+            el.src = "img/нолик.png";
             el.dataset.value = "0";
             hd=1;
             score ++
         }
         else if (hd == 1){
-            el.style.background = "url('img/крестик.png')";
+            el.src = "img/крестик.png";
             el.dataset.value = "1";
             hd=0;
             score ++;
@@ -41,19 +41,25 @@ function hod(el){
     if (((one.dataset.value== "0" && two.dataset.value== "0" && free.dataset.value== "0" ) || (four.dataset.value== "0" && five.dataset.value== "0" && six.dataset.value== "0" )|| (seven.dataset.value== "0" && eich.dataset.value== "0" && nine.dataset.value== "0" )) ||
     ((one.dataset.value== "0" && four.dataset.value== "0" && seven.dataset.value== "0" ) || (two.dataset.value== "0" && five.dataset.value== "0" && eich.dataset.value== "0" )|| (free.dataset.value== "0" && six.dataset.value== "0" && nine.dataset.value== "0" )) ||
     ((one.dataset.value== "0" && five.dataset.value== "0" && nine.dataset.value== "0" ) || (free.dataset.value== "0" && five.dataset.value== "0" && seven.dataset.value== "0" ))) {
-    
+        for (var i = 0; i<grid_cl.length; i++) {
+            grid_cl[i].style.pointerEvents = "none"
+        }
         des_hd.textContent = "Победили нолики";
         var timer = setTimeout(function(){res = confirm("Вы хотите начать сначала?"); restart()}, 3000);
     }
     else if (((one.dataset.value== "1" && two.dataset.value== "1" && free.dataset.value== "1" ) || (four.dataset.value== "1" && five.dataset.value== "1" && six.dataset.value== "1" )|| (seven.dataset.value== "1" && eich.dataset.value== "1" && nine.dataset.value== "1" )) ||
     ((one.dataset.value== "1" && four.dataset.value== "1" && seven.dataset.value== "1" ) || (two.dataset.value== "1" && five.dataset.value== "1" && eich.dataset.value== "1" )|| (free.dataset.value== "1" && six.dataset.value== "1" && nine.dataset.value== "1" )) ||
     ((one.dataset.value== "1" && five.dataset.value== "1" && nine.dataset.value== "1" ) || (free.dataset.value== "1" && five.dataset.value== "1" && seven.dataset.value== "1" ))) {
-       
+       for (var i = 0; i<grid_cl.length; i++) {
+            grid_cl[i].style.pointerEvents = "none"
+        }
         des_hd.textContent = "Победили крестики";
         var timer = setTimeout(function(){res = confirm("Вы хотите начать сначала?"); restart()}, 3000);
     }
     else if (score == 9) {
-        
+        for (var i = 0; i<grid_cl.length; i++) {
+            grid_cl[i].style.pointerEvents = "none"
+        }
         des_hd.textContent = "победила дружба";
         var timer = setTimeout(function(){res = confirm("Вы хотите начать сначала?"); restart()}, 3000);
     } 
@@ -70,9 +76,9 @@ function restart(){
         if (hd == 1) des_hd.textContent = "Ходит Крестик";
         score = 0;
         for (var i = 0; i<grid_cl.length; i++) {
-            grid_cl[i].style.background = "url('img/пустая клетка.png')";
+            grid_cl[i].src = "img/пустая клетка.png";
             grid_cl[i].dataset.value = "3";
-
+            grid_cl[i].style.pointerEvents = "auto"
         }
     }
     else {window.location.href = "../../game_home1.html"}
